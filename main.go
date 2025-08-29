6045a896519b0a7ec091323e3d1da073a4884b2b
kajal-jotwani <kajaljotwani06@gmail.com>
2025-08-29 23:36:06 +0530

    initialize skeleton of the project

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	var folder string
+	var email string
+
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git Repositories")
+	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+
+	flag.Parse()
+
+	if folder != "" {
+		scan(folder)
+		return
+	}
+
+	stats(email)
+}
